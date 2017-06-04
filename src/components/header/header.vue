0880125114db4be9bdfa933064b3f48bfe939b23
yangdm2015 <yangshan503@126.com>
2017-06-04 16:49:11 +0800

    header complited

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -29,31 +29,41 @@
     <div class="background">
       <img :src="seller.avatar" alt="" width="100%" height="100%">
     </div>
-    <div v-show="detailShow" class="detail">
-      <div class="detail-wrapper">
-        <div class="detail-main">
-          <h1 class="name">{{seller.name}}</h1>
-          <div class="star-wrapper">
-            <star :size="48" :score="seller.score"></star>
+    <transition name="fade">
+      <div v-show="detailShow" class="detail" >
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="item in seller.supports">
+                <span class="icon" :class="classMap[item.type]"></span>
+                <span class="text">{{item.description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <div class="content">{{seller.bulletin}}</div>
+            </div>
           </div>
-          <div class="title">
-            <div class="line"></div>
-            <div class="text">优惠信息</div>
-            <div class="line"></div>
-          </div>
-          <span class="icon decrease"></span>
-          <ul v-if="seller.supports" class="support">
-            <li class="support-item" v-for="item in seller.supports">
-              <span class="icon" :class="classMap[item.type]">{{item.type}}</span>
-              <span class="text">{{item.description}}</span>
-            </li>
-          </ul>
+        </div>
+
+        <div class="detail-close" @click="hideDetail">
+          <i class="icon-close"></i>
         </div>
       </div>
-      <div class="detail-close">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -67,12 +77,16 @@
     },
     data(){
       return {
-        detailShow: false
+        detailShow: false,
+        clear: false
       };
     },
     methods: {
       showDetail(){
         this.detailShow = true;
+      },
+      hideDetail(){
+        this.detailShow = false;
       }
     },
     created(){
@@ -205,6 +219,12 @@
       height: 100%
       overflow: auto
       background: rgba(7 17 27 0.8)
+      &.fade-enter-active, &.fade-leave-active
+        transition: opacity .5s
+      &.fade-enter,&.fade-leave-active
+        opacity: 0
+      /*&.fade-leave
+        opacity: 1*/
       .detail-wrapper
         min-height: 100%
         .detail-main
@@ -232,6 +252,46 @@
               padding: 0 12px
               font-weight: 700
               font-size: 14px
+
+          .supports
+            width: 80%
+            margin: 0 auto
+            .support-item
+              padding: 0 12px
+              margin-bottom: 12px
+              font-size: 0
+              &:last-child
+                margin-bottom: 0
+              .icon
+                display: inline-block
+                width: 16px
+                height: 16px
+                vertical-align: top
+                margin-right: 6px
+                background-size: 16px 16px
+                background-repeat: no-repeat
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
+
+              .text
+                line-height: 16px
+                font-size: 12px
+
+          .bulletin
+            width: 80%
+            margin: 0 auto
+            .content
+              padding: 0 12px
+              line-height: 24px
+              font-size: 12px
       .detail-close
         position: relative
         width: 32px
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
+import App from './App.vue';
 import VueResource from 'vue-resource';
 /* import router from './router'; */
 import VueRouter from 'vue-router';
@@ -14,10 +14,10 @@ Vue.use(VueRouter);
 Vue.use(VueResource);
 Vue.config.productionTip = false;
 const routes = [
-  { path: '/', component: goods },
-  { path: '/goods', component: goods },
-  { path: '/ratings', component: ratings },
-  { path: '/seller', component: seller }
+  {path: '/', component: goods},
+  {path: '/goods', component: goods},
+  {path: '/ratings', component: ratings},
+  {path: '/seller', component: seller}
 ];
 const router = new VueRouter({
   routes: routes,
@@ -29,6 +29,6 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App, goods }
+  components: {App, goods}
 });
 
